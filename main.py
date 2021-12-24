fabc93f5bf34a768765f00e42ca9de20d54bf6d9
Wiley <wileystphillips@gmail.com>
2021-12-23 23:47:14 -0500

    Work on add_alarm() and clean gui

--- main.py
+++ main.py
@@ -8,49 +8,70 @@ root.configure(bg=bgColor)
 
 
 def set_screen():
-    global gui, text_entry, am_pm_entry, amPm
+    global gui, hours_entry, minutes_entry, am_pm_entry, amPm, alarms
     amPm = "am"
-    text_entry = StringVar()
-    text_entry.set("N/A")
-    am_pm_entry = StringVar()
-    am_pm_entry.set(amPm)
+    hours_entry = StringVar()
+    hours_entry.set("")
+    minutes_entry = StringVar()
+    minutes_entry.set("")
     alarms = [
         Label(root, text="       ")
     ]
     gui = [
         Label(root, text="Next Alarm"),
-        Label(root, textvariable=text_entry),
-        Entry(root),
-        Entry(root),
-        Label(root, textvariable=am_pm_entry),
-        Button(root, text="^"),
-        Button(root, text="v"),
-        Button(root, text="^"),
-        Button(root, text="v"),
-        Button(root, text="^", command=time_of_day)
+        Label(root, text="N/A"),
+        Entry(root, textvariable=hours_entry),
+        Entry(root, textvariable=minutes_entry),
+        Label(root, text="am"),
+        Button(root, text="^", command=time_of_day),
+        Button(root, text="Submit", command=add_alarm)
     ]
     gui[0].grid(row=0, column=2)
     gui[1].grid(row=1, column=2)
     gui[2].grid(row=4, column=1)
     gui[3].grid(row=4, column=2)
     gui[4].grid(row=4, column=3)
-    gui[5].grid(row=3, column=1)
-    gui[6].grid(row=5, column=1)
-    gui[7].grid(row=3, column=2)
-    gui[8].grid(row=5, column=2)
-    gui[9].grid(row=3, column=3)
+    gui[5].grid(row=3, column=3)
+    gui[6].grid(row=4, column=4)
     alarms[0].grid(row=2, column=0)
 
 
-# ERROR need to get label text
 def time_of_day():
     global amPm
     if amPm == "am":
         amPm = "pm"
-        am_pm_entry.set(amPm)
+        gui[4].configure(text=amPm)
     else:
         amPm = "am"
-        am_pm_entry.set(amPm)
+        gui[4].configure(text=amPm)
+
+
+def add_alarm():
+    global amPm
+    successful_alarm = True
+    try:
+        hours = int(gui[2].get())
+        if hours > 12:
+            hours = "12"
+            hours_entry.set(hours)
+        hours = str(hours)
+    except:
+        print("Hours except")
+        successful_alarm = False
+        hours_entry.set("")
+    try:
+        minutes = int(gui[3].get())
+        if minutes > 59:
+            minutes = "59"
+            minutes_entry.set(minutes)
+        minutes = str(minutes)
+    except:
+        print("Minutes except")
+        successful_alarm = False
+        minutes_entry.set("")
+    new_alarm = "{}:{}{}".format(gui[2].get(), gui[3].get(), amPm)
+    if successful_alarm:
+        alarms[0].configure(text=new_alarm)
 
 
 root.title("Alarm")
